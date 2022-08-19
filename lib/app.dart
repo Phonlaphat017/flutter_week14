f9811c1a3a415305a112d9c7d3c92f95e8da7149
Phonlapha Tabwongsri <phonlaphat9826@gmail.com>
2022-08-19 16:21:44 +0700

    up

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'contact.dart';
-import 'home.dart';
-import 'list_view_demo.dart';
-import 'list_viem_builder.dart';
-import 'list_view_separate.dart';
-import 'list_view_meru.dart';
+import 'list_tile_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key); //1.constructor
 
   @override
-  Widget build(BuildContext buildContext) {
-    //2.build
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListViewMeru(),
+      home: ListTilePage(
+
+      ),
     );
   }
 }
